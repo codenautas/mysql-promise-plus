bd5395aa68770343d9d68d8ac37833f7e9c8873a
Diego F.(i7) <diegoefe@gmail.com>
2015-09-22 16:19:20 -0300

    Modifico prepareConnection() para que pueda crear y borrar la base de datos

--- test/test.js
+++ test/test.js
@@ -4,6 +4,7 @@ var Promises = require('promise-plus');
 var tester=require('sql-promise-tester');
 var Path = require('path');
 var winOS = Path.sep==='\\';
+var _ = require('lodash');
 
 var MotorMysql = require('..').Motor;
 
@@ -21,15 +22,15 @@ if(! winOS) {
 
 function prepareConnection(){
     var conn;
-    return MotorMysql.connect(defaultConnOpts).then(function(con) {
+    var conOpts = _.clone(defaultConnOpts);
+    // conectamos con esta base para poder hacer el drop database!!
+    conOpts.database = 'information_schema';
+    return MotorMysql.connect(conOpts).then(function(con) {
         conn = con;
-        // esto por ahora no funciona si no existe la base de datos
-        // tal vez haya que conectar inicialmente con mysql y luego borrar y crear la base
         return MotorMysql.prepare(con, "DROP DATABASE IF EXISTS "+defaultConnOpts.database);
     }).then(function(query) {
         return MotorMysql.query(query);
     }).then(function(query) {
-        //console.log("prepareConnection", result);
         return MotorMysql.fetchAll(query);
     }).then(function(res) {
         return MotorMysql.prepare(conn, "CREATE DATABASE "+defaultConnOpts.database);
@@ -38,7 +39,6 @@ function prepareConnection(){
     }).then(function(query) {
         return MotorMysql.fetchAll(query);
     }).then(function(res) {
-    }).then(function() {
         MotorMysql.done(conn);
     });
 }
