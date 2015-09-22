2454bf1f9434624f2daa2ecf47f7861727210796
Diego F.(i7) <diegoefe@gmail.com>
2015-09-22 11:48:06 -0300

    Actualizo deps. Agrego create.sql. Ya corre el test hasta connect

--- test/test.js
+++ test/test.js
@@ -1,55 +1,57 @@
 "use strict";
 
+var Promises = require('promise-plus');
+var tester=require('sql-promise-tester');
 var Path = require('path');
 var winOS = Path.sep==='\\';
 
 var MotorMysql = require('..').Motor;
 
 var defaultConnOpts = {
+  motor      :'test',
   host       : 'localhost',
   user       : 'node',
   password   : 'edon',
-  database   : 'nodepru'
+  database   : 'nodepru',
+  port       : MotorMysql.defaultPort
 };
 if(! winOS) {
     defaultConnOpts['socketPath']= '/var/run/mysql/mysql.sock';
 }
 
 function prepareConnection(){
-    var dbFile = defaultConnOpts.file;
-    return fs.unlink(dbFile).catch(function(err){
-        if(err.code!=='ENOENT'){ throw err; }
-    }).then(function(){
-        MotorSqlite.connect(defaultConnOpts);
+    var conn;
+    return MotorMysql.connect(defaultConnOpts).then(function(con) {
+        conn = con;
+        return MotorMysql.prepare(con, "DROP DATABASE IF EXISTS "+defaultConnOpts.database);
+    }).then(function(query) {
+        return MotorMysql.query(query);
+    }).then(function(query) {
+        //console.log("prepareConnection", result);
+        return MotorMysql.fetchAll(query);
+    }).then(function(res) {
+        return MotorMysql.prepare(conn, "CREATE DATABASE "+defaultConnOpts.database);
+    }).then(function(prep) {
+        return MotorMysql.query(prep);
+    }).then(function(query) {
+        return MotorMysql.fetchAll(query);
+    }).then(function(res) {
+    }).then(function() {
+        MotorMysql.done(conn);
     });
 }
 
-tester(MotorSqlite, {
+var badConnOpts = {
+  motor      :'test',
+  host       : 'localhostage',
+  user       : 'nodeja',
+  password   : 'edonista',
+  database   : 'nodeprumal'
+};
+
+tester(MotorMysql, {
     connOpts: defaultConnOpts, 
-    badConnOpts: 'inexis_file.db', 
-    prepare:prepareConnection
+    badConnOpts: badConnOpts, 
+    prepare:prepareConnection,
+    testUntil:'connect'
 });
-
-/*
-var mysqlPMS = require('..');
-
-var motor = new mysqlPMS.Motor;
-var conn=null;
-motor.connect(defaultConnOpts).then(function(con) {
-    // console.log("******************* con", con);
-    //return motor.prepare(con, "CREATE table t1(id integer, name varchar);");
-    conn = con;
-    return motor.prepare(con, "SELECT * from t1");
-}).then(function(prepared) {
-    // console.log("******************* prepared", prepared);
-    return motor.query(prepared, "");
-}).then(function(query) {
-    //console.log("******************* query", query.conn);
-    return motor.fetchAll(query);
-}).then(function(result) {
-    console.log("******************* result", result);
-    motor.close(conn);
-}).catch(function(err) {
-    console.log("******************* err", err.stack);
-});
-*/
